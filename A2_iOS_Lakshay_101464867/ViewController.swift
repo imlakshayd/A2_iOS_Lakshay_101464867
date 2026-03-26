7b7c038149a784890c6276871c5824a407a783a7
Lakshay <lakshay101464867@georgebrown.ca>
2026-03-26 10:00:00 -0400

    Initial Xcode project setup with Core Data support

---
+++ A2_iOS_Lakshay_101464867/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  A2_iOS_Lakshay_101464867
+//
+//  Created by Lakshay on 2026-03-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
